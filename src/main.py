3775ccd764aa8e2f02dda5fc8ec17c0536112a17
Jinwon Kim <sesac.jwkim@gmail.com>
2026-06-15 17:15:02 +0900

    과제제출1

--- src/main.py
+++ src/main.py
@@ -10,8 +10,12 @@
 #
 # 선형 비례 관계로 계산하세요.
 def servo_angle_to_pulse_width(angle):
-    raise NotImplementedError
-
+    if 0 <= angle <= 180:
+        pulse_width = round(500 + (angle * 2000 / 180))
+        return pulse_width
+    else:
+        print(f"[경고] 올바르지 않은 각도 입력: {angle}° (0~180도만 가능)")
+        return 0
 
 # 문제 2.
 #
@@ -19,8 +23,19 @@ def servo_angle_to_pulse_width(angle):
 #
 # C4, D4, E4, F4, G4, A4, B4, C5 음계를 지원해야 합니다.
 def note_to_frequency(note):
-    raise NotImplementedError
-
+    notes = [
+    ("C4", 262),
+    ("D4", 294),
+    ("E4", 330),
+    ("F4", 349),
+    ("G4", 392),
+    ("A4", 440),
+    ("B4", 494),
+    ("C5", 523),
+]
+    note_dict = dict(notes)
+    
+    return note_dict.get(note, 0)
 
 # 문제 3.
 #
@@ -29,7 +44,13 @@ def note_to_frequency(note):
 # notes 인자는 음계 이름이 들어있는 리스트입니다.
 # 각 음계를 주파수 값으로 변환한 리스트를 반환하세요.
 def melody_to_frequencies(notes):
-    raise NotImplementedError
+    note_melody = {
+        "C4": 262, "D4": 294, "E4": 330, "F4": 349,
+        "G4": 392, "A4": 440, "B4": 494, "C5": 523
+    }
+    
+    return [note_melody[note] for note in notes]
+
 
 
 # 문제 4.
@@ -48,7 +69,20 @@ def melody_to_frequencies(notes):
 #
 # 반환값은 (linear_x, angular_z) 튜플입니다.
 def direction_to_twist(direction):
-    raise NotImplementedError
+    if direction == "forward":
+        return (1.0, 0.0)
+    elif direction == "backward":
+        return (-1.0, 0.0)
+    elif direction == "left":
+        return (0.0, 1.0)
+    elif direction == "right":
+        return (0.0, -1.0)
+    elif direction == "stop":
+        return (0.0, 0.0)
+    else :
+        return (0.0, 0.0)
+
+
 
 
 # 문제 5.
@@ -62,4 +96,11 @@ def direction_to_twist(direction):
 #
 # 반환값은 (left_speed, right_speed) 튜플입니다.
 def twist_to_wheel_speed(linear_x, angular_z):
-    raise NotImplementedError
+    
+    left_speed = (linear_x - angular_z)*100
+    right_speed = (linear_x + angular_z)*100
+
+    left_speed = max(-100, min(100, left_speed))
+    right_speed = max(-100, min(100, right_speed))
+    
+    return (left_speed, right_speed)
